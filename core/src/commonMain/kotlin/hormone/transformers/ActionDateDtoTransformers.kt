a2151dab5153e574253c0f624ece73f2bffb32ef
andylamax <andylamax@programmer.net>
2023-07-19 16:51:20 +0300

    Implemented https://linear.app/picortex/issue/CORE-354/date-time-formatter-for-invoices

--- core/src/commonMain/kotlin/hormone/transformers/ActionDateDtoTransformers.kt
+++ core/src/commonMain/kotlin/hormone/transformers/ActionDateDtoTransformers.kt
@@ -2,11 +2,12 @@ package hormone.transformers
 
 import hormone.ActionDateDto
 import hormone.ActionDatePresenter
+import krono.PresenterPattern
 import krono.PureDateTimeFormatter
 import krono.TimeZone
 import krono.toDateTimePresenter
 
-fun ActionDateDto.toPresenter(tz: TimeZone, formatter: PureDateTimeFormatter) = ActionDatePresenter(
-    created = created.toDateTimePresenter(tz, formatter),
-    updated = updated.toDateTimePresenter(tz, formatter)
+fun ActionDateDto.toPresenter(tz: TimeZone, pattern: PresenterPattern) = ActionDatePresenter(
+    created = created.toDateTimePresenter(tz, pattern),
+    updated = updated.toDateTimePresenter(tz, pattern)
 )
