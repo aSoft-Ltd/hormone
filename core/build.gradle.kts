0a2e7dcc47704c1c8ba28a8799ca1960e78bc6aa
andylamax <andylamax@programmer.net>
2024-02-22 07:36:42 +0300

    Merging build.gradle.kts

--- core/build.gradle.kts
+++ core/build.gradle.kts
@@ -9,7 +9,7 @@ description = "A collections of interoperable functions to easy interoperabiliy 
 kotlin {
     jvm { library() }
     if (Targeting.JS) js(IR) { library() }
-    if (Targeting.WASM) wasmJs { library() }
+//    if (Targeting.WASM) wasmJs { library() }
 //    if (Targeting.WASM) wasmWasi { library() }
     val osxTargets = if (Targeting.OSX) osxTargets() else listOf()
 //    val ndkTargets = if (Targeting.NDK) ndkTargets() else listOf()
